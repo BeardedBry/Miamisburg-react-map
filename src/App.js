c4e5e60ebc88543526c3700ef3c72ea9bbb48a88
Brian Burkard <brianburkard@gmail.com>
2019-02-10 20:20:40 -0500

    marker and info window

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 
 const mapStyles = {
   width: '100%',
   heightL: '100%'
 }
-
 export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  }
+
+  onMarkerClick = (props, marker, e) => 
+  this.setState({
+    selectedPlace: props,
+    activeMarker: marker,
+    showingInfoWindow: true
+  });
+
+  onClose = props => {
+    if(this.state.showingInfoWindow){
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  }
+
+
   render() {
     return (
       <Map
-      google={this.props.google}
-      zoom={17}
-      style={mapStyles}
-      initialCenter = {{
-        lat: 39.6417034,
-        lng: -84.2889354
-      }}
-      />
+        google={this.props.google}
+        zoom={17}
+        style={mapStyles}
+        initialCenter = {{lat: 39.6417034,lng: -84.2889354}}
+      >
+        <Marker
+          onClick={this.onMarkerClick}
+          name={'Miamisburg, Ohio'}
+        />
+        <InfoWindow
+        marker={this.state.activeMarker}
+        visible={this.state.showingInfoWindow}
+        onClose={this.onClose}
+        >
+          <div>
+           <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </Map>
     );
   }
 }
@@ -25,3 +58,4 @@ export class MapContainer extends Component {
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyCcQ4sUMPkgiYOBLeFoEyPZ7kMyT53K2C4'
 })(MapContainer);
+//https://scotch.io/tutorials/react-apps-with-the-google-maps-api-and-google-maps-react
